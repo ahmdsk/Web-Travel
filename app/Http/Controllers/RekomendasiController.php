2b8082ab9b03b82d4acb09362c124684a1e412a9
Ahmad Shaleh <ahmadsaleh2409@gmail.com>
2022-09-09 13:47:31 +0700

    create text to undrstnad this

--- app/Http/Controllers/RekomendasiController.php
+++ app/Http/Controllers/RekomendasiController.php
@@ -13,10 +13,13 @@ class RekomendasiController extends Controller
         $similar = array();
         $sum = 0;
 
+        // data pembilang, penyebut dipecah dan di ulang
         foreach ($preferences[$person1] as $key => $value) {
+            // kemudian jika iduser sama dengan user yang lain
             if (array_key_exists($key, $preferences[$person2]))
+                // maka similar nya dibuat 1 jika tidak maka 0
                 $similar[$key] = 1;
-            $nonsimilar[$key] = 0;
+                $nonsimilar[$key] = 0;
         }
 
         if (count($similar) == 0)
@@ -30,38 +33,6 @@ class RekomendasiController extends Controller
         return  1 / (1 + sqrt($sum));
     }
 
-
-    public function matchItems($preferences, $person)
-    {
-        $score = array();
-
-        foreach ($preferences as $otherPerson => $values) {
-            if ($otherPerson !== $person) {
-                $sim = $this->similarityDistance($preferences, $person, $otherPerson);
-
-                if ($sim > 0)
-                    $score[$otherPerson] = $sim;
-            }
-        }
-
-        array_multisort($score, SORT_DESC);
-        return $score;
-    }
-
-    public function transformPreferences($preferences)
-    {
-        $result = array();
-
-        foreach ($preferences as $otherPerson => $values) {
-            foreach ($values as $key => $value) {
-                $result[$key][$otherPerson] = $value;
-            }
-        }
-
-        return $result;
-    }
-
-
     public function getRecommendations($preferences, $person)
     {
         $total = array();
@@ -70,7 +41,10 @@ class RekomendasiController extends Controller
         $sim = 0;
 
         foreach ($preferences as $otherPerson => $values) {
+            // jika user yang sedang login tidak sama dengan user lain
             if ($otherPerson != $person) {
+                // maka panggil function dibawah untuk mencari nilai rata"
+                // isi nya ($preferences = nilai[idUser][idPesanan]/Pembilang-Penyebut)
                 $sim = $this->similarityDistance($preferences, $person, $otherPerson);
             }
 
@@ -126,50 +100,56 @@ class RekomendasiController extends Controller
                             ->where('id_user', $u->id)
                             ->select('rating')->pluck('rating')->first();
     
+                    // $u->id = id user dari semua user yang telah memesan travel
+                    // $psn->id_pesanan = id pesanan yang telah dipesan oleh semua user
+                    // $rating = dapat dari rating yang telah semua user berikan
+
                     $dataRekomendasi[$u->id][$psn->id_pesanan] = $rating;
                 }   
             }
 
+            // panggil function untuk cek data pembilang, dan penyebut serta user yang ingin di cek (user login)
             $rekomendasi = $this->getRecommendations($dataRekomendasi, $userLogin->id);
 
-            // foreach($rekomendasi as $idProduk => $rating){
-            //     $produkRekomendasi   = DB::table('destinasi')
-            //                 ->join('tblmobil', 'tblmobil.id_mobil', '=', 'destinasi.mobil_id')
-            //                 ->join('tbldriver', 'tbldriver.id_driver', '=', 'tblmobil.driver_id')
-            //                 ->join('tblperusahaan', 'tblperusahaan.id_perusahaan', '=', 'tbldriver.perusahaan_id')
-            //                 ->orderBy('destinasi.id', 'desc')
-            //                 ->get();
+            foreach($rekomendasi as $idpesanan => $rating){
+                $produkRekomendasi   = DB::table('destinasi')
+                            ->join('tblmobil', 'tblmobil.id_mobil', '=', 'destinasi.mobil_id')
+                            ->join('tbldriver', 'tbldriver.id_driver', '=', 'tblmobil.driver_id')
+                            ->join('tblperusahaan', 'tblperusahaan.id_perusahaan', '=', 'tbldriver.perusahaan_id')
+                            ->join('tblpemesanan', 'tblpemesanan.destinasi_id', '=', 'destinasi.id')
+                            ->where('id_pesanan', $idpesanan)
+                            ->get();
 
-            //     // foreach($produkRekomendasi as $pr){
-            //     //     $dataRating = DB::table('product_ratings')->where('id_product', $pr->id)->pluck('ratings');
+                foreach($produkRekomendasi as $pr){
+                    $dataRating = DB::table('tblrating')->where('id_pesanan', $pr->id_pesanan)->pluck('rating');
                 
-            //     //     if(count($dataRating) > 0){
-            //     //         array_push($data['rekomendasi'], [
-            //     //             'id'            => $pr->id,
-            //     //             'product_name'	=> $pr->product_name,
-            //     //             'product_price'	=> $pr->product_price,
-            //     //             'category_id'   => $pr->category_id,
-            //     //             'gambar'	    => $pr->gambar,
-            //     //             'jenis_gambar'	=> $pr->jenis_gambar,
-            //     //             'jumlah_rating' => [
-            //     //                 'rating'    => array_sum(DB::table('product_ratings')->where('id_product', $pr->id)->pluck('ratings')->toArray()) / DB::table('product_ratings')->where('id_product', $pr->id)->count()
-            //     //             ]
-            //     //         ]);
-            //     //     }else{
-            //     //         array_push($data['rekomendasi'], [
-            //     //             'id'            => $pr->id,
-            //     //             'product_name'	=> $pr->product_name,
-            //     //             'product_price'	=> $pr->product_price,
-            //     //             'category_id'   => $pr->category_id,
-            //     //             'gambar'	    => $pr->gambar,
-            //     //             'jenis_gambar'	=> $pr->jenis_gambar,
-            //     //             'jumlah_rating' => [
-            //     //                 'rating'    => 0
-            //     //             ]
-            //     //         ]);
-            //     //     }
-            //     // }
-            // }
+                    if(count($dataRating) > 0){
+                        array_push($data['rekomendasi'], [
+                            'id'            => $pr->id,
+                            'product_name'	=> $pr->product_name,
+                            'product_price'	=> $pr->product_price,
+                            'category_id'   => $pr->category_id,
+                            'gambar'	    => $pr->gambar,
+                            'jenis_gambar'	=> $pr->jenis_gambar,
+                            'jumlah_rating' => [
+                                'rating'    => array_sum(DB::table('tblrating')->where('id_pesanan', $pr->id_pesanan)->pluck('rating')->toArray()) / DB::table('tblrating')->where('id_pesanan', $pr->id_pesanan)->count()
+                            ]
+                        ]);
+                    }else{
+                        array_push($data['rekomendasi'], [
+                            'id'            => $pr->id,
+                            'product_name'	=> $pr->product_name,
+                            'product_price'	=> $pr->product_price,
+                            'category_id'   => $pr->category_id,
+                            'gambar'	    => $pr->gambar,
+                            'jenis_gambar'	=> $pr->jenis_gambar,
+                            'jumlah_rating' => [
+                                'rating'    => 0
+                            ]
+                        ]);
+                    }
+                }
+            }
         }else{
             $rekomendasi = [];
         }
